3a674991ff8ae167fb106afbec76a86c42947e98
Rafael <rafael.r.carvalho@edu.ufes.br>
2025-03-17 11:43:09 -0300

    Jhipster implementation

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -3,14 +3,13 @@ import { Router, RouterOutlet } from '@angular/router';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
-import FooterComponent from '../footer/footer.component';
 import PageRibbonComponent from '../profiles/page-ribbon.component';
 
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent],
+  imports: [RouterOutlet, PageRibbonComponent],
 })
 export default class MainComponent implements OnInit {
   private readonly router = inject(Router);
